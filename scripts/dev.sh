2c66053e52b553a932c08cda0c31712adbc3f9c3
Ramit Mittal <commitemailoframit@protonmail.com>
2022-06-17 22:16:45 +0530

    fix dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -12,7 +12,7 @@ else
   cp src/manifest.json dist/manifest.json
 fi
 
-parcel watch src/browser_action/index.html src/content_scripts/main.js src/background/index.html --out-dir dist/ &
+parcel watch src/browser_action/index.html src/content_scripts/main.js src/background/index.html --dist-dir dist/ &
 parcelProcess=$!
 
 sleep 3
